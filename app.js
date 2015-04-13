e13d6138713ff50d1d3f54c7293f0c33daffa836
mjunaidsalaat <mjunaidsalaat@gmail.com>
2015-04-13 18:16:18 +0500

    Allow Cross Origin Access MiddleWare added

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-/*//Allow Cross Origin Access MiddleWare
+//Allow Cross Origin Access MiddleWare
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("'Access-Control-Allow-Methods',['OPTIONS', 'GET', 'POST', 'DELETE']");
   res.header("'Access-Control-Allow-Headers','Content-Type'");
   next();
-});*/
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
